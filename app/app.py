cfb89977e5949e1aa3faddd43d643f26e28409e2
reiul <rmarcelinodocio@gmail.com>
2024-06-25 09:55:34 +0200

    Subo cambios

--- app/app.py
+++ app/app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import MonthLocator, DateFormatter
+import os
+import io
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    csv = os.path.join(os.path.dirname(__file__), 'forecast.csv')
+    df = pd.read_csv(csv)
+    df['ds'] = pd.to_datetime(df['ds'])
+
+    ranges = [(0.8, 2, 'red', 1),
+              (0.6, 0.8, 'orange', 1),
+              (0.4, 0.6, 'yellow', 1),
+              (0.2, 0.4, 'green', 1),
+              (-1, 0.2, 'blue', 1)]  
+
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(20,6))
+
+    ax.plot(df['ds'], df['yhat'],  linestyle='-', color='w')
+    fechas_a_pintar = df[df['yhat'] < 0.34]['ds']
+    for ymin, ymax, color, alpha in ranges:
+        fechas_a_pintar = df[(df['yhat'] >= ymin) & (df['yhat'] < ymax)]['ds']
+        for date in fechas_a_pintar:
+            ax.axvspan(date, date + pd.Timedelta(days=1), color=color, alpha=1)
+    plt.title('Predicción de estado')
+
+    # Guardar el gráfico en un archivo temporal
+    img_bytes = io.BytesIO()
+    plt.savefig(img_bytes, format='png')
+    img_bytes.seek(0)
+
+    # Limpiar la figura para liberar memoria
+    plt.close()
+
+    # Mostrar la imagen en la página web
+    return render_template('index.html', image=img_bytes)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/scrap24h.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime, timedelta, date
+import os
+import io
+
+# URL de la página a scrapear
+url = 'http://www.intecmar.gal/Informacion/biotoxinas/Evolucion/CierresBatea.aspx'
+
+# Realizar la solicitud GET a la URL
+response = requests.get(url)
+
+# Verificar que la solicitud fue exitosa (código 200)
+if response.status_code == 200:
+    # Parsear el contenido HTML con BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Encontrar la tabla por su ID (puedes inspeccionar el HTML para obtener el ID correcto)
+    table = soup.find('table', {'id': 'ctl00_Contenido_GridView2'})
+    
+    if table:
+        # Extraer los datos de la tabla y convertirlos a un DataFrame de pandas
+        df_hoy = pd.read_html(str(table))[0]
+                
+        df_hoy = df_hoy.rename(columns={'Xan': '1', 'Feb': '2', 'Mar': '3', 'Abr': '4', 'Mai': '5', 'Xun': '6', 'Xull': '7', 'Ago': '8', 'Set': '9', 'Out': '10', 'Nov': '11', 'Dec': '12'})
+        df_hoy = df_hoy.fillna('0')
+
+        df_hoy = df_hoy[df_hoy['Polígono'] != '0']
+
+        script_dir = os.path.dirname(__file__) 
+
+        hoy = date.today()
+        ayer_fecha = hoy - timedelta(1)
+        mes = hoy.month
+        mes = str(mes)
+        ayer_fecha = str(ayer_fecha)
+        
+        csv = os.path.join(os.path.dirname(__file__), 'data', 'abiertocerrado_scrap.csv')
+        df2 = pd.read_csv(csv)
+        
+        csv = os.path.join(os.path.dirname(__file__), 'data', 'diascierre_ayer.csv')
+        df_ayer = pd.read_csv(csv)
+
+        existe_valor = (df2['dia'] == ayer_fecha).any()
+        if not existe_valor:
+            for index, row_ayer in df_ayer.iterrows():
+                polig = row_ayer['Polígono']
+                df_hoy_poligono = df_hoy[df_hoy['Polígono'] == polig]
+                v_hoy = df_hoy_poligono[mes].iloc[0]
+                df_ayer_poligono = df_ayer[df_ayer['Polígono'] == polig]
+                v_ayer = df_ayer_poligono[mes].iloc[0]
+                v_hoy = int(v_hoy)
+                v_ayer = int(v_ayer)
+
+                existe_valor = (df2['dia'] == ayer_fecha).any()
+
+                if v_hoy != v_ayer:
+                    if existe_valor:
+                        df2[polig] = '1'
+                    else: 
+                        nueva_fila = {'dia' : ayer_fecha, polig : '1'}
+                        df2.loc[len(df2)] = nueva_fila
+                else:
+                    if existe_valor:
+                        df2[polig] = '0'
+                    else: 
+                        nueva_fila = {'dia' : ayer_fecha, polig : '0'}
+                        df2.loc[len(df2)] = nueva_fila
+
+            file_path = os.path.join(script_dir,'data','abiertocerrado_scrap.csv')
+            df2.to_csv(file_path, index=False)
+
+            file_path = os.path.join(script_dir,'data','diascierre_ayer.csv')
+            df_hoy.to_csv(file_path, index=False)
+    else:
+        print('No se encontró la tabla en la página.')
+else:
+    print('No se pudo acceder a la página.')
