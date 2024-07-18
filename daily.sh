f709229071ba57caa3a4be419e2a881a69aa987e
Ubuntu <ubuntu@ip-172-31-32-133.eu-west-3.compute.internal>
2024-07-18 10:09:26 +0000

    auto: cambios diarios

--- daily.sh
+++ daily.sh
@@ -8,4 +8,6 @@ python /home/ubuntu/mytilus/mytilus2/app/forecast24h.py
 python /home/ubuntu/mytilus/mytilus2/app/scrap24h.py
 
 #Actualizar git
+git add .
+git commit -m "auto: cambios diarios"
 git push origin main
